fe718b772c413aa5faf1b6922a0a7e23d1df17ac
hyeonggeun2 <dhgudrms12@gmail.com>
2019-12-11 17:36:39 +0900

    bootstrap CSS파일 추가
    - STATICFILES_DIRS설정 추가
    - static폴더 추가
    - static/bootstrap폴더 내에 bootstrap관련 CSS파일들 추가
    - post_list.html에서 bootstrap CSS파일 link태그 추가

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
 
 def post_list(request):
     # Template을 찾을 경로에서 '입력한 .html' 파일을 찾아서
     # 텍스트로 만들어서 HttpResponse 형태로 돌려줌
-    return render(request, 'post_list.html')
+
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'post_list.html', context)
--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    # djangogirls/static 폴더 path를 추가
+    os.path.join(BASE_DIR, 'static')
+]
